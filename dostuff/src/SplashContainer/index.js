18b26f0171ce1e1857813010929e81b03d43b1d5
Steve <sbrennison@gmail.com>
2018-08-20 17:08:47 -0500

    even more merging

--- dostuff/src/SplashContainer/index.js
+++ dostuff/src/SplashContainer/index.js
@@ -2,36 +2,46 @@ import React, { Component } from 'react';
 import Login from './Login';
 
 
-// FETCH LINKS ARE MISSING!!!!!!!!!!!!!!!!!!!!
-
-
 
 class SplashContainer extends Component {
   constructor(){
     super();
     this.state = {
-      login: '',
+      username: '',
       password: '',
       // skipLogin: null
     }
 
     getCategories = async () => {
-      const categories = await fetch('', {
+      const categories = await fetch('http://localhost:8000/api/events', {
         credentials: 'include',
         method: 'GET'
       });
       // const parsedCategories = categories.json();
 
     handleSubmit = async (e) => {
-    e.preventDefault();
-    const loginResponse = await fetch('', {
-      method: 'POST',
-      credentials: 'include',
-      body: JSON.stringify(this.state),
-      headers: {
-        'Content-Type': 'application/json'
+      e.preventDefault();
+      const loginResponse = await fetch('http://localhost:8000/api/login', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(this.state),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const loginResponseJSON = loginResponse.json();
+
+      if(loginResponseJSON.status === 200){
+        // redirect to home page
+        this.props.history.push('/events');
+      } else {
+        // login false - send error message to user
       }
-    });
+
+    }
+
+    
 
     handleChange = (e) => {
       this.setState({[e.target.name]: e.target.value});
@@ -50,7 +60,7 @@ class SplashContainer extends Component {
               <form onSubmit={this.handleSubmit}>
                 <label>
                   Username:
-                  <input name='login' placeholder='Username' onChange={this.handleChange} />
+                  <input name='username' placeholder='Username' onChange={this.handleChange} />
                 </label>
                 <label>
                   Password:
