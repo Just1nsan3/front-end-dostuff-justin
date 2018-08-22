21bec263cdb19005943ad18a46c93a90f52552bb
Steve <sbrennison@gmail.com>
2018-08-22 14:36:51 -0500

    more styling and merging

--- dostuff/src/Header/index.js
+++ dostuff/src/Header/index.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const Header = () => {
   return(
     <header className='header'>
-      <img src='./logo_transparent.png' alt='logo' height='120' width='120'/>
+      <a href='/'><img src='./logo_transparent.png' alt='logo' height='120' width='120'/></a>
       <Menu />
     </header>
   )
