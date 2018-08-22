51496c174f67cc462f15488310928b030193fa09
Badal Moradia <badal@i360llc.com>
2018-08-22 14:58:37 -0500

    added menu changes

--- dostuff/src/Header/index.js
+++ dostuff/src/Header/index.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const Header = () => {
   return(
     <header className='header'>
-      <a href='/'><img src='./logo_transparent.png' alt='logo' height='120' width='120'/></a>
+      <Link to='/'><img src='./logo_transparent.png' alt='logo' height='120' width='120'/></Link>
       <Menu />
     </header>
   )
