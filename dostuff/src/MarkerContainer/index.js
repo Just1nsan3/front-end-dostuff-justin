8cd7705fc7d7426e0f6f7a13de44a4294d9455a7
Justin Evangelista <justinevangelista@Justins-MacBook-Pro.local>
2018-08-29 16:23:35 -0500

    got markers to show info window with correct name from yelp api

--- dostuff/src/MarkerContainer/index.js
+++ dostuff/src/MarkerContainer/index.js
@@ -1,36 +1,80 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from 'react-google-maps';
 
 
 
-const MarkerContainer = (props) => {
-	const markers = props.allEvents.map((event, i) => {
+class MarkerContainer extends Component {
+    constructor(props){
+    	super(props);
 
-		return(
-			<Marker key={i}
-					position={{
-						lat: event.latitude,
-						lng: event.longitude
-					}}
-			>
-			<InfoWindow><h1>Please Show up</h1></InfoWindow>
-			</Marker>
-		)
-	})
-	return(
-		
 
-			<GoogleMap
-		    	center={{lat:41.881832, lng: -87.623177}}
-		    	zoom={10}
-		    >
-		    	{markers}
-		  	</GoogleMap>
 
-	  	
+    	this.state = {
+    		isOpen: ""
+    	}
+    }
 
-	)
+	handleToggleOpen = (i) => {
+		this.setState({
+			isOpen: i
+		})
+
+
+		// const state = this.state
+		// // change stuff in state
+		// this.setState(state)
+
+
+    }
+    handleToggleClose = () => {
+        this.setState({
+            isOpen: false
+        })
+    }
+
+    render(){
+
+
+
+
+    	return(
+    	
+        	<GoogleMap
+                center={{lat:41.881832, lng: -87.623177}}
+                zoom={13}
+            >
+	            {this.props.allEvents.map((event, i) => {
+
+        			return(
+			            <Marker key={i}
+			                    position={{
+			                        lat: event.latitude,
+			                        lng: event.longitude
+			                    }}
+			                    onClick={() => this.handleToggleOpen(i)}
+
+			
+			            >
+			            {this.state.isOpen === i ?
+			                 <InfoWindow onCloseClick={this.handleToggleOpen}>
+			                      <h4>{event.name}</h4>
+			                 </InfoWindow> : null
+         				}
+			            </Marker>
+			            )
+					})
+	        	}
+			     
+			        
+	            	
+        	</GoogleMap>
+
+   	 	)
+	}
 }
+
+
+
 
 export default withGoogleMap(MarkerContainer);
 
